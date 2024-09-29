f89fbce7873aedc265cd5a9d57195a3443010c83
SirZebo <2300462@sit.singaporetech.edu.sg>
2024-09-29 17:06:45 +0800

    Feat: Implemented BuildingBlocks for CQRS

---
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace Catalog.API.Product.CreateProduct;
+
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid id);
+internal class CreateProductCommandHandler : 
+    ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    // Business logic to create a product
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Create Product entity from command object
+        // save to data
+        // return CreateProductResult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // TODO
+        // save to database
+        // return result
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container. - builder.AddServices()
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-
+app.MapCarter();
 app.Run();
