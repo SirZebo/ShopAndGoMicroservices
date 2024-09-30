73ff52bbec354c6ce9e284fb506ba0ee191bba6f
SirZebo <2957869g@student.gla.ac.uk>
2024-09-30 22:38:56 +0800

    Feats: Created CreateProduct Feature

--- src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductEndpoint.cs
@@ -1,14 +1,29 @@
-﻿using Carter;
-
-namespace Catalog.API.Product.CreateProduct;
+﻿namespace Catalog.API.Product.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
 public record CreateProductResponse(Guid Id);
+
 public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+        
     }
 }
