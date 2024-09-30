60552d5a4e6b69fd3fe40bdebf1a504b586c3aed
SirZebo <2957869g@student.gla.ac.uk>
2024-10-01 02:49:46 +0800

    Feat: Implement CRUD methods for Catalog

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public Guid Id { get; }
+    public ProductNotFoundException(Guid Id) : base($"Product with id '{Id}' cannot be found!")
+        => this.Id = Id;
+}
