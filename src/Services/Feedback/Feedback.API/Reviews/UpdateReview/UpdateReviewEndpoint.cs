fcfff138ca87ec6730a3c3344aabfdf880663c24
SirZebo <sirzebowurks@gmail.com>
2025-03-16 18:04:18 +0800

    Feats: Added UpdateReview in Feedback Service

--- src/Services/Feedback/Feedback.API/Reviews/UpdateReview/UpdateReviewEndpoint.cs
+++ src/Services/Feedback/Feedback.API/Reviews/UpdateReview/UpdateReviewEndpoint.cs
@@ -1,5 +1,31 @@
-﻿namespace Feedback.API.Reviews.UpdateReview;
+﻿using Feedback.API.Enums;
 
-public class UpdateReviewEndpoint
+namespace Feedback.API.Reviews.UpdateReview;
+
+public record UpdateReviewRequest(Guid Id, FeedbackStatus FeedbackStatus, string Body);
+
+public record UpdateReviewResponse(bool IsSuccess);
+
+public class UpdateReviewEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/reviews", async (UpdateReviewRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateReviewCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateReviewResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateReview")
+        .Produces<UpdateReviewResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Review")
+        .WithDescription("Update Review");
+    }
 }
+
--- src/Services/Feedback/Feedback.API/Reviews/UpdateReview/UpdateReviewHandler.cs
+++ src/Services/Feedback/Feedback.API/Reviews/UpdateReview/UpdateReviewHandler.cs
@@ -1,5 +1,53 @@
-﻿namespace Feedback.API.Reviews.UpdateReview;
+﻿using Feedback.API.Enums;
+using Feedback.API.Exceptions;
+using Feedback.API.Model;
 
-public class UpdateReviewHandler
+namespace Feedback.API.Reviews.UpdateReview;
+
+public record UpdateReviewCommand(Guid Id, FeedbackStatus FeedbackStatus, string Body) : ICommand<UpdateReviewResult>;
+
+public record UpdateReviewResult(bool IsSuccess);
+
+public class UpdateReviewValidator : AbstractValidator<UpdateReviewCommand>
 {
+    public UpdateReviewValidator()
+    {
+        RuleFor(command => command.Id)
+            .NotEmpty().WithMessage("Product Id is required");
+
+        RuleFor(x => x.FeedbackStatus)
+            .NotEmpty().WithMessage("FeedbackStatus is required");
+
+        RuleFor(x => x.Body)
+            .NotEmpty().WithMessage("Body is required")
+            .Length(2, 150).WithMessage("Body must be between 2 and 150 characters");
+    }
+}
+
+internal class UpdateReviewCommandHandler
+    (IDocumentSession session)
+    : ICommandHandler<UpdateReviewCommand, UpdateReviewResult>
+{
+    public async Task<UpdateReviewResult> Handle(UpdateReviewCommand command, CancellationToken cancellationToken)
+    {
+        var review = await session.LoadAsync<Review>(command.Id, cancellationToken);
+
+        if (review is null)
+        {
+            throw new ReviewNotFoundException(command.Id);
+        }
+
+        if (review.FeedbackStatus != FeedbackStatus.Incomplete)
+        {
+            throw new ReviewAlreadySubmittedException(command.Id);
+        }
+
+        review.FeedbackStatus = command.FeedbackStatus;
+        review.Body = command.Body;
+
+        session.Update(review);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateReviewResult(true);
+    }
 }
