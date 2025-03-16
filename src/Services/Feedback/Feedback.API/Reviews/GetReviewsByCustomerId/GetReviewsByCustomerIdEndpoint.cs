7adc2a582d36725a9ec93385756f1ab9a55333dc
SirZebo <2300462@sit.singaporetech.edu.sg>
2025-03-16 16:54:31 +0800

    Feats: GetReviewsByCustomerId in Feedback Service

---
+++ src/Services/Feedback/Feedback.API/Reviews/GetReviewsByCustomerId/GetReviewsByCustomerIdEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Feedback.API.Model;
+using Feedback.API.Reviews.GetReviewsByCustomerId;
+
+namespace Feedback.API.Reviews.GetReviewByCustomer;
+
+// public record GetReviewsByCustomerIdRequest();
+
+public record GetReviewsByCustomerIdResponse(List<Review> Reviews);
+
+public class GetReviewsByCustomerIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/reviews/customer/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetReviewsByCustomerIdQuery(id));
+
+            var response = result.Adapt<GetReviewsByCustomerIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetReviewsByCustomerId")
+        .Produces<GetReviewsByCustomerIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Reviews By CustomerId")
+        .WithDescription("Get Reviews By CustomerId");
+
+    }
+}
---
+++ src/Services/Feedback/Feedback.API/Reviews/GetReviewsByCustomerId/GetReviewsByCustomerIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using Feedback.API.Model;
+
+namespace Feedback.API.Reviews.GetReviewsByCustomerId;
+
+public record GetReviewsByCustomerIdQuery(Guid CustomerId) : IQuery<GetReviewsByCustomerIdResult>;
+
+public record GetReviewsByCustomerIdResult(IEnumerable<Review> Reviews);
+public class GetReviewsByCustomerIdHandler
+{
+}
+internal class GetReviewsByCustomerIdQueryHandler
+    (IDocumentSession session)
+    : IQueryHandler<GetReviewsByCustomerIdQuery, GetReviewsByCustomerIdResult>
+{
+    public async Task<GetReviewsByCustomerIdResult> Handle(GetReviewsByCustomerIdQuery query, CancellationToken cancellationToken)
+    {
+        var reviews = await session.Query<Review>()
+                .Where(x => x.Order.CustomerId == query.CustomerId)
+                .ToListAsync();
+
+        if (reviews.IsEmpty())
+        {
+            //throw new ProductNotFoundException(query.Id);
+        }
+
+        return new GetReviewsByCustomerIdResult(reviews);
+    }
+}
+
