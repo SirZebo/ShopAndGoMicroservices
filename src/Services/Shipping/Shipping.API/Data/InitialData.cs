21b2d41c4b8d8cd2b28b35c08e11657f98c6567f
SirZebo <sirzebowurks@gmail.com>
2025-03-22 14:52:57 +0800

    Feats: Seed Data

--- src/Services/Shipping/Shipping.API/Data/InitialData.cs
+++ src/Services/Shipping/Shipping.API/Data/InitialData.cs
@@ -24,7 +24,25 @@ public class InitialData : IInitialData
                     Order = GetPreconfiguredOrders().ToList()[0],
                     ShipmentStatus = Enums.ShipmentStatus.OrderReceived
                 },
-                
+                new Shipment()
+                {
+                    Id = new Guid("5805c332-ec79-4d6e-a48f-e685d10f0042"),
+                    Order = GetPreconfiguredOrders().ToList()[1],
+                    ShipmentStatus = Enums.ShipmentStatus.OrderReceived
+                },
+                new Shipment()
+                {
+                    Id = new Guid("5805c332-ec79-4d6e-a48f-e685d10f0042"),
+                    Order = GetPreconfiguredOrders().ToList()[2],
+                    ShipmentStatus = Enums.ShipmentStatus.OrderReceived
+                },
+                new Shipment()
+                {
+                    Id = new Guid("5805c332-ec79-4d6e-a48f-e685d10f0042"),
+                    Order = GetPreconfiguredOrders().ToList()[3],
+                    ShipmentStatus = Enums.ShipmentStatus.OrderReceived
+                },
+
 
             };
 
@@ -39,13 +57,65 @@ public class InitialData : IInitialData
                     Quantity = 1,
                     OrderDeadline = DateTime.UtcNow.AddDays(3),
                     Language = "Malay",
-                    FirstName = "mehmet", 
-                    LastName = "ozkaya", 
-                    EmailAddress = "mehmet@gmail.com", 
-                    AddressLine = "Bahcelievler No:4", 
-                    Country = "Turkey", 
-                    State = "Istanbul", 
+                    FirstName = "mehmet",
+                    LastName = "ozkaya",
+                    EmailAddress = "mehmet@gmail.com",
+                    AddressLine = "Bahcelievler No:4",
+                    Country = "Turkey",
+                    State = "Istanbul",
                     ZipCode = "38050"
                 },
+                new Order()
+                {
+                    Id = new Guid("3a34ff51-6106-46b4-84fb-de34e0938867"),
+                    CustomerId = new Guid("58c49479-ec65-4de2-86e7-033c546291aa"),
+                    MerchantId = new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d"),
+                    ProductId = new Guid("3b423421-6dfb-4ec2-a54c-358bc19d30f1"),
+                    Quantity = 1,
+                    OrderDeadline = DateTime.UtcNow.AddDays(3),
+                    Language = "Malay",
+                    FirstName = "John", 
+                    LastName = "Snow", 
+                    EmailAddress = "john@gmail.com", 
+                    AddressLine = "Paya Lebar Blk 123", 
+                    Country = "Singapore", 
+                    State = "Singapore", 
+                    ZipCode = "123456"
+                },
+                new Order()
+                {
+                    Id = new Guid("19ac9cce-f730-41f9-8872-a75ef9c463fa"),
+                    CustomerId = new Guid("58c49479-ec65-4de2-86e7-033c546291aa"),
+                    MerchantId = new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d"),
+                    ProductId = new Guid("3b423421-6dfb-4ec2-a54c-358bc19d30f1"),
+                    Quantity = 1,
+                    OrderDeadline = DateTime.UtcNow.AddDays(3),
+                    Language = "Malay",
+                    FirstName = "John",
+                    LastName = "Snow",
+                    EmailAddress = "john@gmail.com",
+                    AddressLine = "Paya Lebar Blk 123",
+                    Country = "Singapore",
+                    State = "Singapore",
+                    ZipCode = "123456"
+                },
+                new Order()
+                {
+                    Id = new Guid("5805c332-ec79-4d6e-a48f-e685d10f0042"),
+                    CustomerId = new Guid("58c49479-ec65-4de2-86e7-033c546291aa"),
+                    MerchantId = new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d"),
+                    ProductId = new Guid("3b423421-6dfb-4ec2-a54c-358bc19d30f1"),
+                    Quantity = 1,
+                    OrderDeadline = DateTime.UtcNow.AddDays(3),
+                    Language = "Malay",
+                    FirstName = "John",
+                    LastName = "Snow",
+                    EmailAddress = "john@gmail.com",
+                    AddressLine = "Paya Lebar Blk 123",
+                    Country = "Singapore",
+                    State = "Singapore",
+                    ZipCode = "123456"
+                },
+                
             };
 }
